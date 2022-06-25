0d13bf5f8cc2fe8636dff2670ef6c126c8bcf471
Noor-26 <nk26982698@gmail.com>
2022-06-26 02:29:56 +0600

    summery section created

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Details from '../Details/Details'
 import Header from '../Header/Header'
+import Summary from '../Summary/Summary'
 
 function Home() {
   return (
     <div>
       <Header/>
       <Details/>
+      <Summary/>
     </div>
   )
 }
