f1bd0168e98f0bb442d53f017a46f9153a7229c0
Noor-26 <nk26982698@gmail.com>
2022-06-26 20:45:14 +0600

    footer publication section and page created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home/Home';
+import Publications from './Components/Publications/Publications';
+import Footer from './Components/Shared/Footer/Footer';
 import Navber from './Components/Shared/Navber/Navber';
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
       <Navber/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/publications' element={<Publications/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Shared/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+  return (
+    <footer className='footer-container '>
+        <div className='ms-auto me-5 footer-text d-flex align-items-center flex-column justify-content-center '>
+        <h1 className='card-text_header my-5'>JOIN HUNDREDS OF HEALTHY PEOPLE</h1>
+        <p>Begin your journey to a better life with peace, love, beauty & happiness. Put on your comfy clothes and come wiggle your toes with us.</p>
+        </div>
+        <div>
+            <p className='ps-5'>Copyright © 2022 Vyayam Vibes</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
